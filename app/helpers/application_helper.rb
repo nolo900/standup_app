15699499e5a48476b07b0f322371cf4e8e24199f
Austin Miles <abm@austinbmiles.com>
2017-07-18 17:41:40 -0400

    added devise signup and error helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,32 @@
 module ApplicationHelper
+
+  def bootstrap_class_for(flash_type)
+    case flash_type
+      when "notice" then "alert-info"
+      when "success" then "alert-success"
+      when "error" then "alert-danger"
+      when "alert" then "alert-warning"
+    end
+  end
+
+  def flash_messages( _opts = {} )
+
+    flash.each do |msg_type, message|
+      # puts 'msg_type: ', msg_type, 'message: ', message
+      next if msg_type == :form_error
+
+      concat(content_tag(:div, message.html_safe, {
+          class: "alert #{bootstrap_class_for(msg_type)} fade in"}) do
+          concat content_tag(:button, 'x', class: 'close', data: {
+              dismiss:'alert'
+            })
+          concat message.html_safe
+      end)
+
+    end
+
+    nil
+
+  end
+
 end
